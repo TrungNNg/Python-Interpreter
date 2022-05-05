415a8751f2fd2a34b1c993743d4c510185e7312d
TrungNNg <tnguy384@mail.ccsf.edu>
2022-05-04 20:37:57 -0400

    finish tokenzier for p2.in

--- tokenizer/tokenizer.go
+++ tokenizer/tokenizer.go
@@ -17,73 +17,95 @@ type token struct {
 
 // constants to represent token categories
 const (
-    EOF = 0 // end of file
-    PRINT = 1 // 'print' keyword
-    UNSIGNEDINT = 2 // integer
-    NAME = 3 // identifier that is not a keyword
-    ASSIGNOP = 4 // '='
-    LEFTPARENT = 5 // '('
-    RIGHTPARENT = 6 // ')' 
-    PLUS = 7 // '+'
-    MINUS = 8 // '-'
-    TIMES = 9 // '*'
-    NEWLINE = 10 // newline character
-    ERROR = 11 // if not any of the above error
+    EOF = iota // end of file
+    PRINT  // 'print' keyword
+    UNSIGNEDINT // integer
+    NAME // identifier that is not a keyword
+    ASSIGNOP // '='
+    LEFTPARENT // '('
+    RIGHTPARENT // ')' 
+    PLUS // '+'
+    MINUS // '-'
+    TIMES // '*'
+    NEWLINE // newline character
+    ERROR // if not any of the above error
+    NONE
+    TRUE
+    FALSE
+    PASS
+    IF
+    ELSE
+    WHILE
+    UNSIGNEDFLOAT
+    STRING
+    EQUAL
+    NOTEQUAL
+    LESSTHAN
+    LESSEQUAL
+    GREATERTHAN
+    GREATEREQUAL
+    DIV
+    COMMA
+    COLON
+    INDENT
+    DEDENT
 )
-
-var catnames = map[int]string {
-    0 : "EOF",
-    1 : "PRINT",
-    2 : "UNSIGNEDINT",
-    3 : "NAME",
-    4 : "ASSIGNOP",
-    5 : "LEFTPARENT",
-    6 : "RIGHTPARENT",
-    7 : "PLUS",
-    8 : "MINUS",
-    9 : "TIMES",
-    10 : "NEWLINE",
-    11 : "ERROR",
-}
 
 // keywords and their category
 var keyWords = map[string]int {
-    "print" : PRINT,
+    "print" : PRINT, "None" : NONE, "True" : TRUE,
+    "False" : FALSE, "pass" : PASS, "if" : IF,
+    "else" : ELSE, "while" : WHILE,
 }
 
 // one-character tokens and their category
 var smallTokens = map[string]int {
     "=" : ASSIGNOP, "(" : LEFTPARENT, ")" : RIGHTPARENT, "+" : PLUS,
-    "-" : MINUS, "*" : TIMES, "\n" : NEWLINE, "" : EOF,
+    "-" : MINUS, "*" : TIMES, "\n" : NEWLINE, "" : EOF, "==" : EQUAL,
+    "<" : LESSTHAN, "<=" : LESSEQUAL, ">" : GREATERTHAN, ">=" : GREATEREQUAL,
+    "!" : ERROR, "!=" : NOTEQUAL, "," : COMMA, ":" : COLON, "/" : DIV,
 }
 
 // package level var,
 var tokenlist []token // store tokens
 var source string     // string of source file
 var sourceIndex int   // keep track of current character's index in source string
-var prevChar byte     // save previous character
+var prevChar byte = '\n'     // save previous character
 var line int          // line number of current token
 var column int        // column number of current token
-var isBlankLine bool    // check if current line is a bankline 
+var isBlankLine bool = true    // check if current line is a bankline 
+var isInString bool
+var parenLevel int    //
 
 // return the current character pointed by sourceIndex and adjust row and column.
 func getChar() byte {
     if prevChar == '\n' {
-        column = 0
         line += 1
+        column = 0
         isBlankLine = true
     }
 
     // if at the end of source file, change isEOF flag
     if sourceIndex >= len(source) {
         column = 1
-        line += 1
         return 0
     }
 
     c := source[sourceIndex]
     sourceIndex += 1
     column += 1
+
+    // check if # is in string or start of comment
+    if c == '#' && !isInString {
+        for {
+            c = source[sourceIndex]
+            sourceIndex += 1
+            if c == '\n' {
+                break
+            }
+        }
+    }
+
     if !unicode.IsSpace(rune(c)) {
         isBlankLine = false
     }
@@ -99,6 +121,7 @@ func getChar() byte {
 
 func tokenizer() {
     var currChar byte = ' '
+    indentStack := []int{1}
     for {
         // skip white space but not newline
         for currChar != '\n' && unicode.IsSpace(rune(currChar)) {
@@ -106,12 +129,17 @@ func tokenizer() {
         }
 
         var currToken = token{row: line, column: column, category: -1, lexeme: ""} 
-        if unicode.IsDigit(rune(currChar)) {
+        if unicode.IsDigit(rune(currChar)) || currChar == '.'{
             currToken.category = UNSIGNEDINT
+            if currChar == '.' {
+                currToken.category = UNSIGNEDFLOAT
+            }
             for {
                 currToken.lexeme += string(currChar)
                 currChar = getChar()
-                if !unicode.IsDigit(rune(currChar)) {
+                if currToken.category == UNSIGNEDINT && currChar == '.' {
+                    currToken.category = UNSIGNEDFLOAT
+                } else if !unicode.IsDigit(rune(currChar)) {
                     break
                 }
             }
@@ -131,10 +159,49 @@ func tokenizer() {
                 currToken.category = NAME
             }
             
-        } else if v, ok := smallTokens[string(currChar)]; ok {
-            currToken.category = v
-            currToken.lexeme = string(currChar)
+        } else if _, ok := smallTokens[string(currChar)]; ok {
+            saveChar := currChar
             currChar = getChar()
+            twoChar := string(saveChar) + string(currChar)
+            if v, ok := smallTokens[twoChar]; ok {
+                currToken.category = v
+                currToken.lexeme = string(twoChar)
+                currChar = getChar()
+            } else {
+                currToken.category = smallTokens[string(saveChar)]
+                currToken.lexeme = string(saveChar)
+            }
+        } else if currChar == 39 {
+            // current char is single quote ', indicate start of string data
+            isInString = true
+            for {
+                currChar = getChar()
+                if currChar == 0 || currChar == '\n'{
+                    fmt.Println("unterminated string on", line)
+                    os.Exit(1)
+                }
+                if currChar == 39 { // currChar == '
+                    currChar = getChar() // advance pass last single quote
+                    currToken.category = STRING
+                    isInString = false
+                    break
+                }
+                if currChar == 92 { // currChar == \
+                    currChar = getChar()
+                    if currChar == 'n' {
+                        currToken.lexeme += "\n"
+                    } else if currChar == 't' {
+                        currToken.lexeme += "\t"
+                    } else if currChar == '\n' {
+                        currToken.lexeme += "n"
+                    } else {
+                        currToken.lexeme += string(currChar)
+                    }
+                } else {
+                    currToken.lexeme += string(currChar)
+                }
+            }
+
         } else if currChar == 0 {
             currToken.category = EOF
             currToken.lexeme = ""
@@ -144,6 +211,27 @@ func tokenizer() {
             fmt.Println("error occur with token " + string(currChar) + " on line", line)
             os.Exit(1)
         }
+
+        if len(tokenlist) == 0 || tokenlist[len(tokenlist)-1].category == NEWLINE {
+            if indentStack[len(indentStack)-1] < currToken.column{
+                indentStack = append(indentStack, currToken.column)
+                var indentToken = token{row: currToken.row, column: currToken.column, category: INDENT, lexeme : "{"}
+                tokenlist = append(tokenlist, indentToken) 
+            } else if indentStack[len(indentStack)-1] > currToken.column {
+                for {
+                    var dedentToken = token{row: currToken.row, column: currToken.column, category : DEDENT, lexeme : "}"}
+                    tokenlist = append(tokenlist, dedentToken)
+                    indentStack = indentStack[:len(indentStack)-1]
+                    if indentStack[len(indentStack)-1] == currToken.column {
+                        break
+                    } else if indentStack[len(indentStack)-1] < currToken.column {
+                        fmt.Println("indetation err on line", line)
+                        os.Exit(1)
+                    }
+                }
+            }
+        }
+
         tokenlist = append(tokenlist, currToken)
         if currToken.category == EOF {
             break
@@ -179,10 +267,9 @@ func readSourceFile() string {
 func main() {
     // read source from source python file
     source = readSourceFile()
-    fmt.Println(source)
     tokenizer()
     for _, token := range tokenlist {
-        fmt.Println(token.lexeme, catnames[token.category])
+        fmt.Printf("%d %d %s\n", token.row, token.column, string(token.lexeme))
     }
     fmt.Println("length of tokenlist" , len(tokenlist))
 }
