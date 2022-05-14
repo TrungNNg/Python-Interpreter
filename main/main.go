414991f3c584febb6b4d522b5996d1313f8313e1
TrungNNg <tnguy384@mail.ccsf.edu>
2022-05-13 21:25:21 -0400

    finish interpreter for i2.in

--- main/main.go
+++ main/main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"os"
 	. "test1/tokenizer"
-	//"strconv"
+	"strconv"
+	r "reflect"
 )
 
 // global var
@@ -41,6 +42,84 @@ func stackPop() interface{} {
 	save := stack[len(stack)-1]
 	stack = stack[:len(stack)-1]	// slice to remove last element
 	return save
+}
+
+// conver boolean value to 1 or 0
+func btoi(b bool) int {
+	if b {return 1}
+	return 0
+}
+
+
+// take 2 interface{} and return comparision result
+func compare(x, y interface{}, compOp string) bool {
+	s1, xIsStr := x.(string)
+	s2, yIsStr := y.(string)
+
+	//fmt.Println(s1, s2)
+	if s1 == "None" || s2 == "None" {
+		if s1 == s2 || compOp == "!=" {
+			return true
+		}
+		return false
+	}
+
+	if xIsStr && yIsStr {
+		// switch
+		switch compOp {
+		case "==":
+			return s1 == s2
+		case "!=":
+			return s1 != s2
+		case "<":
+			return s1 < s2
+		case "<=":
+			return s1 <= s2
+		case ">":
+			return s1 > s2
+		case ">=":
+			return s1 >= s2
+		}
+		
+	} else if xIsStr || yIsStr {   // 1 string 1 number
+		if compOp == "==" || compOp == "!=" {
+			return false
+		} else {
+			panic("Compare string with number.")
+		}
+	}
+
+	var xv float64
+	var yv float64
+	if r.ValueOf(x).CanFloat() {
+		xv = r.ValueOf(x).Float()
+	} else { 								// unsafe
+		xv = float64(x.(int))
+	}
+
+	if r.ValueOf(y).CanFloat() {
+		yv = r.ValueOf(y).Float()
+	} else { 								// unsafe
+		yv = float64(y.(int))
+	}
+
+	// switch
+	switch compOp {
+		case "==":
+			return xv == yv
+		case "!=":
+			return xv != yv
+		case "<":
+			return xv < yv
+		case "<=":
+			return xv <= yv
+		case ">":
+			return xv > yv
+		case ">=":
+			return xv >= yv
+		}
+
+	return false // death code, never execute
 }
 
 
@@ -112,9 +191,9 @@ func simplestmt() {
 		returnstmt()
 	case GLOBAL:
 		globalstmt()
-	default:
-		fmt.Println("Expecting statement, got", token.Lexeme)
-		os.Exit(1)
+	// default:
+	// 	fmt.Println("Expecting statement, got", token.Lexeme)
+	// 	os.Exit(1)
 	}
 }
 
@@ -133,32 +212,43 @@ func compoundstmt() {
 
 // <assignmentstmt> -> NAME "=" <relexpr>
 func assignmentstmt() {
-	//left := token.Lexeme
+	left := token.Lexeme
 	advance() // simplestmt() already check this token is NAME
 	consume(ASSIGNOP) 
 	relexpr()
-	//symtab[left] = stackPop()
+	symtab[left] = stackPop()
 }
 
 // <printstmt> -> "print" "(" [<relexpr> (COMMA <relexpr>)* [COMMA]] ")"
 func printstmt() {
 	advance()
-	//fmt.Println("current token lexeme", token.Lexeme)
 	consume(LEFTPARENT)
-	//fmt.Println("current token lexeme after (", token.Lexeme)
 	if token.Category != RIGHTPARENT {
 		relexpr()
+		v := stackPop()
+		f,isFloat := v.(float64)
+		if isFloat {
+			fmt.Printf("%.1f", f)
+		}else {
+			fmt.Print(v)
+		}
 		for token.Category == COMMA {
 			// there are 2 cases:  ,e OR ,)
-			if tokenlist[tokenIndex + 1].Category == RIGHTPARENT {
-				advance()	
-			} else {
-				advance()
-				relexpr()
+			advance()
+			if token.Category == RIGHTPARENT {
+				break;
+			}
+			relexpr()
+			v = stackPop()
+			f,isFloat = v.(float64)
+			if isFloat {
+				fmt.Printf("%0f", f)
+			}else {
+				fmt.Print(" ",v)
 			}
 		}
 	}
-	//fmt.Println(stackPop())
+	fmt.Println()
 	consume(RIGHTPARENT)
 }
 
@@ -203,20 +293,73 @@ func ifstmt() {
 	advance()
 	relexpr()
 	consume(COLON)
-	codeblock()
+	saveVal := stackPop()
+	v,ok := saveVal.(int); 
+	//fmt.Println(v, " ", ok)
+	if ok && v == 1 {
+		//fmt.Println("if is true")
+		codeblock()
+	} else {
+		consume(NEWLINE)
+		indentCol := token.Column
+		consume(INDENT)
+		for {
+			if token.Category == DEDENT && token.Column < indentCol {
+				advance()
+				break
+			}
+			advance()
+		}
+	}
+
 	if token.Category == ELSE {
 		advance()
 		consume(COLON)
-		codeblock()
+		if v, ok := saveVal.(int) ; !ok || v != 1 {
+			codeblock()
+		} else {
+			consume(NEWLINE)
+			indentCol := token.Column
+			consume(INDENT)
+			for {
+				if token.Category == DEDENT && token.Column < indentCol {
+					advance()
+					break
+				}
+				advance()
+			}
+		}
 	}
 }
 
 // <whilestmt> -> "while" "(" <relexpr> ")" ":" <codeblock>
 func whilestmt() {
 	advance()
-	relexpr()
-	consume(COLON)
-	codeblock()
+	saveIndex := tokenIndex
+	for {
+		relexpr()
+		consume(COLON)
+		v,ok := stackPop().(int)
+		//fmt.Println(v, " ", ok) // 1, true and 1 true
+		if ok && v == 1 {
+			//fmt.Println("While hit")
+			codeblock()
+			tokenIndex = saveIndex
+			token = tokenlist[tokenIndex]
+		} else {
+			break
+		}
+	}
+	consume(NEWLINE)
+	indentCol := token.Column
+	consume(INDENT)
+	for {
+		if token.Category == DEDENT && token.Column < indentCol {
+			advance()
+			break
+		}
+		advance()
+	}
 }
 
 // defstmt -> DEF NAME "(" [NAME ("," NAME)*] ")" ":" <codeblock>
@@ -240,7 +383,7 @@ func defstmt() {
 func codeblock() {
 	consume(NEWLINE)
 	consume(INDENT)
-	stmt() // must have at least 1 stmt
+	//stmt() // must have at least 1 stmt
 	m := map[int]bool{PRINT:true,NAME:true,IF:true,WHILE:true,PASS:true, GLOBAL:true, RETURN:true,}
     for _,ok := m[token.Category]; ok; ok = m[token.Category] {
     	stmt()
@@ -252,31 +395,75 @@ func codeblock() {
 func expr() {
 	term()
 	for token.Category == PLUS || token.Category == MINUS {
+		saveCat := token.Category
 		advance()
 		term()
-		/*
 		rightop := stackPop()
 		leftop := stackPop()
-		if token.Category == PLUS{
-			stack = append(stack, rightop.(int) + leftop.(int))
-		} else {
-			stack = append(stack, rightop.(int) - leftop.(int))
+
+		ri, risInt := rightop.(int)
+		li, lisInt := leftop.(int)
+		rs, risStr := rightop.(string)
+		ls, lisStr := leftop.(string)
+		rf, risFloat := rightop.(float64)
+		lf, lisFloat := leftop.(float64)
+		if saveCat == PLUS {
+			if risInt && lisInt {
+				stack = append(stack, ri + li)
+			} else if risFloat && lisFloat {
+				stack = append(stack, rf + lf)
+			} else if risStr && lisStr {
+				stack = append(stack, ls + rs)  // string concat
+			} else if lisInt && risFloat {
+				stack = append(stack, float64(li) + rf)
+			} else if lisFloat && risInt {
+				stack = append(stack, lf + float64(ri))
+			} else if risStr || lisStr {
+				panic("addition between string and number")
+			}
+		} else if saveCat == MINUS {
+			if risInt && lisInt {
+				stack = append(stack, li - ri)
+			} else if risFloat && lisFloat {
+				stack = append(stack, lf - rf)
+			} else if risStr && lisStr {
+				panic("minus two strings")
+			} else if lisInt && risFloat {
+				stack = append(stack, float64(li) - rf)
+			} else if lisFloat && risInt {
+				stack = append(stack, lf - float64(ri))
+			} else if lisStr || risStr {
+				panic("minus between string and number")
+			}
 		}
-		*/
 	}
 }
 
 // <relexpr> -> <expr> [CONDITIONALOP <expr>]
 func relexpr() {
+	m := map[int]bool{EQUAL:true,NOTEQUAL:true,LESSTHAN:true,LESSEQUAL:true,
+					  GREATERTHAN:true,GREATEREQUAL:true,}
 	expr()
-
-	switch token.Category {
-	case EQUAL, NOTEQUAL, LESSTHAN, LESSEQUAL, GREATERTHAN, GREATEREQUAL:
+	if _,ok := m[token.Category]; ok {
+		saveCat := token.Category
 		advance()
 		expr()
-	//default:
-		// fmt.Println("Expecting relational operator, got", token.Lexeme)
-		// os.Exit(1)
+		rightop := stackPop()
+		leftop := stackPop()
+		switch saveCat {
+		case EQUAL:
+			stack = append(stack, btoi(compare(leftop, rightop, "==")))
+		case NOTEQUAL:
+			stack = append(stack, btoi(compare(leftop, rightop, "!=")))
+		case LESSTHAN:
+			stack = append(stack, btoi(compare(leftop, rightop, "<")))
+		case LESSEQUAL:
+			stack = append(stack, btoi(compare(leftop, rightop, "<=")))
+		case GREATERTHAN:
+			stack = append(stack, btoi(compare(leftop, rightop, ">")))
+		case GREATEREQUAL:
+			stack = append(stack, btoi(compare(leftop, rightop, ">=")))
+		}
 	}
 }
 
@@ -284,19 +471,64 @@ func relexpr() {
 func term() {
 	sign = 1
 	factor()
+	//fmt.Println("token lexeme in term ", token.Lexeme)
 	for token.Category == TIMES || token.Category == DIV {
+		saveCat := token.Category
 		advance()
 		sign = 1
 		factor()
-		/*
 		rightop := stackPop()
 		leftop := stackPop()
-		if token.Category == TIMES {
-			stack = append(stack, rightop.(int) * leftop.(int))
-		} else {
-			stack = append(stack, rightop.(int) / leftop.(int))   // integer div need float div
+
+		ri, risInt := rightop.(int)
+		li, lisInt := leftop.(int)
+		rs, risStr := rightop.(string)
+		ls, lisStr := leftop.(string)
+		rf, risFloat := rightop.(float64)
+		lf, lisFloat := leftop.(float64)
+		if saveCat == TIMES {
+			if risInt && lisInt {
+				stack = append(stack, ri * li)
+			} else if risFloat && lisFloat {
+				stack = append(stack, rf * lf)
+			} else if risStr && lisStr {
+				panic("multiply string sequense")
+			} else if lisInt && risFloat {
+				stack = append(stack, float64(li) * rf)
+			} else if lisFloat && risInt {
+				stack = append(stack, lf * float64(ri))
+			} else if risStr || lisStr {
+				// string multiplication, "a" * 3 = "aaa"
+				if risStr {
+					buf := ""
+					for i := 0; i < li; i++ {
+						buf += rs
+					}
+					stack = append(stack, buf)
+				} else if lisStr {
+					buf := ""
+					for i := 0; i < ri ; i++ {
+						buf += ls
+					}
+					stack = append(stack, buf)
+				}
+			}
+		} else if saveCat == DIV {
+			if risInt && lisInt {
+				stack = append(stack, li /ri)
+			} else if risFloat && lisFloat {
+				stack = append(stack, lf / rf)
+			} else if risStr && lisStr {
+				panic("divide string sequense")
+			} else if lisInt && risFloat {
+				stack = append(stack, float64(li) / rf)
+			} else if lisFloat && risInt {
+				stack = append(stack, lf / float64(ri))
+			} else if risStr || lisStr {
+				panic("divide string and number")
+			}
 		}
-		*/
+		
 	}
 }
 
@@ -324,47 +556,69 @@ func factor() {
 		sign = -sign
 		advance()
 		factor()
+	} else if token.Category == TIMES {
+		advance()
+		factor()
+	} else if token.Category == DIV {
+		advance()
+		factor()
 	} else if token.Category == UNSIGNEDINT {
-		//i, _ := strconv.Atoi(token.Lexeme)       // need handle error here
-		//stack = append(stack, sign * i)
+		i, _ := strconv.Atoi(token.Lexeme)       // need handle error here
+		stack = append(stack, sign * i)
 		advance()
 	} else if token.Category == UNSIGNEDFLOAT {
+		f, _ := strconv.ParseFloat(token.Lexeme, 64)
+		stack = append(stack, float64(sign) * f)
 		advance()
 	} else if token.Category == NAME {
 		// check if this var is declared (in symtab)
 		// if it declared push value from symtab to stack
-		/*
-		if v, ok := symtab[token.Lexeme]; ok {
-			stack = append(stack, sign * v.(int))  // will panic if not int
-		} else {
-			fmt.Printf("Name %s not declared\n", token.Lexeme)
-			os.Exit(1)
-		}
-		*/
-
+		
 		// 2 cases NAME or function call
 		if tokenlist[tokenIndex+1].Category == LEFTPARENT {
 			functioncall()
 		} else if tokenlist[tokenIndex+1].Category != LEFTPARENT {
+
+			if v, ok := symtab[token.Lexeme]; ok {
+				i, isInt := v.(int)
+				f, isFloat := v.(float64)
+				if isInt {
+					stack = append(stack, sign * i)
+				} else if isFloat {
+					stack = append(stack, float64(sign) * f)
+				}
+			} else {
+				fmt.Printf("Name %s not declared\n", token.Lexeme)
+				os.Exit(1)
+			}
 			advance()
 		}
 	} else if token.Category == LEFTPARENT {
-		//saveSign := sign
+		saveSign := sign
 		advance()
-		relexpr()
-		/*
+		relexpr()	
 		if saveSign == -1 {
-			stack[len(stack)-1] = -stack[len(stack)-1].(int)
+			i, isInt := stack[len(stack)-1].(int)
+			f, isFloat := stack[len(stack)-1].(float64)
+			if isInt {
+				stack[len(stack)-1] = -i
+			} else if isFloat {
+				stack[len(stack)-1] = -f
+			}
 		}
-		*/
 		consume(RIGHTPARENT)
 	} else if token.Category == STRING {
+		stack = append(stack, token.Lexeme) // stack now contain string
 		advance()
 	} else if token.Category == TRUE {
+		stack = append(stack, 1)			// 1 represent true
 		advance()
 	} else if token.Category == FALSE {
+		stack = append(stack, 0)			// 0 represent false
 		advance()
 	} else if token.Category == NONE {
+		// None only == None and everything else is false
+		stack = append(stack, "None")
 		advance()
 	} else if token.Category == INPUT {
 		advance()
@@ -380,20 +634,13 @@ func factor() {
 		fmt.Println("Expecting factor")
 		os.Exit(1)
 	}
+	//fmt.Println(stack)
 }
 
 /*
-	p3 -> i2 -> i3
+	i2 -> i3
 
-	build parser level 3
-
-    p3 grammar include: 
-	<functiondef> -> DEF NAME "(" [<argumentlist>] ")" ":" <codeblock> 
-	global -> GLOBAL <argumentlist> NL?
-	<functioncall> -> NAME "(" [<argumentlist>] ")" NL? 
-	return -> RETURN NL?
-	build-in function , input() -> INPUT "(" STRING ")" NL? 
-	type conversion , int() -> INT "(" <relexpr> ")"
+	now that p3 is done, next work on i2, check if i1 still work by uncomment
 */
 
 /*
@@ -410,10 +657,10 @@ func main() {
 	BuildTokenList()
 	tokenlist = TokenList
 	//PRINT token list
-	// for _, token := range tokenlist {
- //         fmt.Printf("%d %d %s\n", token.Row, token.Column, token.Lexeme)
- //    }
- //    fmt.Println("length of tokenlist" , len(tokenlist))
+	 // for _, token := range tokenlist {
+  //         fmt.Printf("%d %d %s\n", token.Row, token.Column, token.Lexeme)
+  //    }
+  //    fmt.Println("length of tokenlist" , len(tokenlist))
 	
 
 	// advance to initialize token to tokenlist[0] 
